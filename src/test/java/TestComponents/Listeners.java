408f3cf394a8d1952056fb7a69d151a7c40e7836
marko.sovilj@gmail.com <marko.sovilj@gmail.com>
2024-05-15 17:40:25 +0200

    fifth commit

---
+++ src/test/java/TestComponents/Listeners.java
@@ -0,0 +1,87 @@
+package TestComponents;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+public class Listeners extends TestComponents implements ITestListener{
+	
+	ExtentReports extent = TestComponents.ExtentReporter();
+	ExtentTest test;
+	
+	WebDriver driver;
+	
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		test = extent.createTest(result.getMethod().getMethodName());
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		test.log(Status.PASS, "Test pass");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		test.log(Status.FAIL, "Test fail");
+		test.fail(result.getThrowable());
+		
+//		try {
+//			this.driver = (WebDriver) result.getTestClass().getRealClass().getField("driver").get(result.getInstance());
+//		} catch (Exception e1){
+//			e1.printStackTrace();
+//		}
+//		
+//		String filePath = null;
+//		try {
+//			filePath = getScreenshot(result.getMethod().getMethodName(), driver);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		test.addScreenCaptureFromPath(filePath, result.getMethod().getMethodName());
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		extent.flush();
+	}
+	
+}
--- src/test/java/TestComponents/TestComponents.java
+++ src/test/java/TestComponents/TestComponents.java
@@ -1,23 +1,59 @@
 package TestComponents;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.asserts.SoftAssert;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class TestComponents {
+	
+	
 
 	public WebDriver Initialization() {
 //		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-
+		
+//		this.driver = driver;
+		
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().window().maximize();
 		return driver;
 	}
+	
 
 	public SoftAssert softAssertions() {
 		SoftAssert soft = new SoftAssert();
 		return soft;
+	}
+	
+	public static ExtentReports ExtentReporter() {
+		String path = System.getProperty("user.dir") + "//reports//index.html";
+		
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Report");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Marko Sovilj");
+		
+		return extent;
+	}
+	
+	public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
+		FileUtils.copyFile(source, file);
+		return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
 	}
 
 
